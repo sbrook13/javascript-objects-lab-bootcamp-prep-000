d83ad71983a2b1926a69019522338b204944b1b0
Shelley Brook <sbrook13@gmail.com>
2020-06-27 22:45:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var recipes = {
   "salt" :"1 cup"
 }
 function updateObjectWithKeyAndValue(food,a,b){
-  food[a]=b
-  var newRecipe = Object.assign({},food,{food[a]=b})
+  var newRecipe = Object.assign({},food,{food.a=b}),
   return newRecipe
 }
